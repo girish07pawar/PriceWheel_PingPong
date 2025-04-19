9ef48151d3e12d8374db2cf3ca2f344960c288f8
girish07pawar <111pawargirish@gmail.com>
2025-04-19 16:44:07 +0530

    final mod

--- PongServices.py
+++ PongServices.py
@@ -45,4 +45,4 @@ async def startup_event():
     asyncio.create_task(pong_ping_task())
 
 if __name__ == "__main__":
-    uvicorn.run("pong_service:app", host="0.0.0.0", port=8000)
+    uvicorn.run("PongServices:app", host="0.0.0.0", port=8000)
